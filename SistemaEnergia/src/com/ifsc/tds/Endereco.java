d06a6a4d354770c4f28114873e86dd50fbb892c8
aluno <aluno@172.16.3.17>
2022-12-13 14:38:31 -0300

    Endereço com atributos

--- SistemaEnergia/src/com/ifsc/tds/Endereco.java
+++ SistemaEnergia/src/com/ifsc/tds/Endereco.java
@@ -1,5 +1,70 @@
 package com.ifsc.tds;
 
 public class Endereco {
+	
+	private String rua;
+	
+	private int numero;
+	
+	private String cep;
+	
+	private String cidade;
+	
+	private String Estado;
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return Estado;
+	}
+
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+	
+	public String toString() {
+		String texto = "";
+		
+		texto = "-------------------------"
+				+ "\n Rua: " + getRua()
+				+ "\n Número: " + getNumero()
+				+ "\n Cep: " + getCep()
+		        + "\n Cidade: " + getCidade()
+		        + "\n Estado: " + getEstado();
+		
+		return texto;
+	}
+	
+	
 
 }
