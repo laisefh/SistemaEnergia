483eb8c31486eea6ad48b9de5aca48bd780afc1c
aluno <aluno@172.16.3.17>
2022-12-13 14:21:22 -0300

    Generalização cliente pessoas

--- SistemaEnergia/src/com/ifsc/tds/PessoaFisica.java
+++ SistemaEnergia/src/com/ifsc/tds/PessoaFisica.java
@@ -1,5 +1,23 @@
 package com.ifsc.tds;
 
-public class PessoaFisica {
+public class PessoaFisica extends Cliente {
+	
+	private String CPF;
 
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	
+	public String toString() {
+		String texto = "";
+		
+		texto = "-------------------------"
+				+ "\n CPF: " + getCPF();
+		
+		return texto;
+	}
 }
--- SistemaEnergia/src/com/ifsc/tds/PessoaJuridica.java
+++ SistemaEnergia/src/com/ifsc/tds/PessoaJuridica.java
@@ -1,5 +1,24 @@
 package com.ifsc.tds;
 
-public class PessoaJuridica {
+public class PessoaJuridica extends Cliente{
+	
+	private String CNPJ;
 
+	public String getCNPJ() {
+		return CNPJ;
+	}
+
+	public void setCNPJ(String cNPJ) {
+		CNPJ = cNPJ;
+	}
+	
+	public String toString() {
+		String texto = "";
+		
+		texto = "-------------------------"
+				+ "\n CPF: " + getCNPJ();
+		
+		return texto;
+	}
+	
 }
