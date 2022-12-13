25cc70e4a1dffde3ebc7f13b7150a77bc808ffb4
aluno <aluno@172.16.3.15>
2022-12-13 14:53:09 -0300

    atualização cliente

--- SistemaEnergia/src/com/ifsc/tds/Cliente.java
+++ SistemaEnergia/src/com/ifsc/tds/Cliente.java
@@ -2,12 +2,21 @@ package com.ifsc.tds;
 
 public class Cliente {
 
+	private int clienteId;
 	private String nome;
+	private Endereco endereco;
 	
 	public Cliente() {
 		
 	}
 
+	public int getClienteId() {
+		return clienteId;
+	}
+
+	public void setClienteId(int clienteId) {
+		this.clienteId = clienteId;
+	}
 
 	public String getNome() {
 		return nome;
@@ -16,7 +25,24 @@ public class Cliente {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String toString() {
+		String texto = "";
+		
+		texto = "-------------------------"
+				+ "\n ID Cliente: " + getClienteId()
+				+ "\n Nome: " + getNome()
+				+ "\n Endereço: " + getEndereco();
+		
+		return texto;
+	}
 	
 }
